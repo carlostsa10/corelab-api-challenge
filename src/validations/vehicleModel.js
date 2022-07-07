82a688d82f1a1bcf2c104330749b6f7792a04e02
Carlos Tavares <carlostsa10@gmail.com>
2022-07-07 13:01:12 -0300

    refactoring

--- src/validations/vehicleModel.js
+++ src/validations/vehicleModel.js
@@ -5,7 +5,7 @@ const VehicleSchema = yup.object().shape({
   name: yup.string().max(40).trim().required(),
   brand: yup.string().max(20).trim().required(),
   color: yup.string().max(10).trim().required(),
-  year: yup.number().max(4).required(),
+  year: yup.number().min(1900).required(),
   plate: yup.string().max(7).trim(),
   price: yup.number().min(3),
   favorite: yup.boolean(),
