ce8d7bf0f691c1e96920b0ed84645ba78a045d32
Carlos Tavares <carlostsa10@gmail.com>
2022-07-07 12:52:49 -0300

    refactoring

--- src/validations/vehicleModel.js
+++ src/validations/vehicleModel.js
@@ -1,16 +1,15 @@
 const yup = require('./yup')
+const { object } = require('yup')
 
 const VehicleSchema = yup.object().shape({
   name: yup.string().max(40).trim().required(),
   brand: yup.string().max(20).trim().required(),
   color: yup.string().max(10).trim().required(),
   year: yup.number().max(4).required(),
-  plate: yup.string().max(7).trim().required(),
-  price: yup.number().min(3).required(),
+  plate: yup.string().max(7).trim(),
+  price: yup.number().min(3),
   favorite: yup.boolean(),
-  createAt: yup.date().required(),
+  createAt: yup.date().,
 })
 
-module.exports = {
-  VehicleSchema,
-}
+module.exports = VehicleSchema
